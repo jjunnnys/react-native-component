ce1899a13562d17376dcb2f30acbcda1d1e56395
ppby <salswnss@gmail.com>
2020-09-21 16:12:11 +0900

    Initial RN project setting

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <View>
+          <Text>hello world</Text>
+        </View>
+        <View>
+          <Text>hello world</Text>
+        </View>
+        <View>
+          <Text>hello world</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default App;
